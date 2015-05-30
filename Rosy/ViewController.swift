d1946858964f069f82dd293a8a3a6b3d681c36e2
Jay Greene <docgreene1@Arthur-2.attlocal.net>
2015-05-30 12:04:00 -0400

    Initial Commit

---
+++ Rosy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rosy
+//
+//  Created by Jay Greene on 5/30/15.
+//  Copyright (c) 2015 Jay Greene. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ RosyTests/RosyTests.swift
@@ -0,0 +1,36 @@
+//
+//  RosyTests.swift
+//  RosyTests
+//
+//  Created by Jay Greene on 5/30/15.
+//  Copyright (c) 2015 Jay Greene. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class RosyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
